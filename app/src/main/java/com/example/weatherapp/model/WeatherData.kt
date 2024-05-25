df1a42246887b561e7571981315b379983d442c7
rudkarol <2pir13@gmail.com>
2024-05-25 21:52:30 +0200

    serializable WeatherData data class

--- app/src/main/java/com/example/weatherapp/model/WeatherData.kt
+++ app/src/main/java/com/example/weatherapp/model/WeatherData.kt
@@ -1,7 +1,52 @@
 package com.example.weatherapp.model
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class WeatherData()
+data class WeatherData(
+    val location: Location,
+    val current: Current,
+    val forecast: Forecast
+)
 
+@Serializable
+data class Location(
+    val name: String,
+    val region: String,
+    val country: String
+)
+
+@Serializable
+data class Current(
+    @SerialName("last_updated_epoch") val lastUpdatedEpoch: Long,
+    @SerialName("temp_c") val tempC: Double,
+    @SerialName("temp_f") val tempF: Double,
+    val condition: Condition
+)
+
+@Serializable
+data class Condition(
+    val text: String,
+    val icon: String
+)
+
+@Serializable
+data class Forecast(
+    @SerialName("forecastday") val forecastDay: List<ForecastDay>
+)
+
+@Serializable
+data class ForecastDay(
+    @SerialName("date_epoch") val dateEpoch: Long,
+    val day: Day
+)
+
+@Serializable
+data class Day(
+    @SerialName("maxtemp_c") val maxTempC: Double,
+    @SerialName("maxtemp_f") val maxTempF: Double,
+    @SerialName("mintemp_c") val minTempC: Double,
+    @SerialName("mintemp_f") val minTempF: Double,
+    val condition: Condition
+)
