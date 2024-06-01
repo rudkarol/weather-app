f6987d4129f939ae41d9ec306b3fab8582a73791
rudkarol <2pir13@gmail.com>
2024-06-01 14:14:47 +0200

    location permission request

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.weatherapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
--- app/src/main/java/com/example/weatherapp/ui/WeatherApp.kt
+++ app/src/main/java/com/example/weatherapp/ui/WeatherApp.kt
@@ -1,9 +1,15 @@
 package com.example.weatherapp.ui
 
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.LocationOn
@@ -18,6 +24,8 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -25,8 +33,11 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
 import com.example.weatherapp.model.WeatherData
 import com.example.weatherapp.network.WeatherApi
 import kotlinx.coroutines.launch
@@ -39,17 +50,50 @@ fun WeatherApp() {
     fun getWeatherUpdate(location: String) {
         coroutineScope.launch {
             val newConditions = getWeather(location)
-
             if (newConditions != null) {
                 conditions = newConditions
             }
         }
     }
 
-    Scaffold(
-        topBar = { TopBar(conditions) { getWeatherUpdate(it) } }
-    ) { innerPadding ->
-        Content(conditions, innerPadding) { getWeatherUpdate(it) }
+    val context = LocalContext.current
+    var hasLocationPermission by remember { mutableStateOf(false) }
+
+    val permissionLauncher = rememberLauncherForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        hasLocationPermission = isGranted
+
+        if (isGranted) {
+            getWeatherUpdate("Krakow")  // Replace with actual location
+        }
+    }
+
+    LaunchedEffect(key1 = context) {
+        val permissionStatus = ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION)
+
+        if (permissionStatus == PackageManager.PERMISSION_GRANTED) {
+            hasLocationPermission = true
+            getWeatherUpdate("Opole")  // Replace with actual location
+        } else {
+            permissionLauncher.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
+        }
+    }
+
+    if (hasLocationPermission) {
+        Scaffold(
+            topBar = { TopBar(conditions) { getWeatherUpdate(it) } }
+        ) { innerPadding ->
+            Content(conditions, innerPadding) { getWeatherUpdate(it) }
+        }
+    } else {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text("App requires location permission to function")
+        }
     }
 }
 
